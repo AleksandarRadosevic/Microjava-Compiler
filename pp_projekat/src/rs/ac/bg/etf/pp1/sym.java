e638f62d83435976f878272e9a9928a87999c81e
Aleksandar Radošević <aradosevic40@gmail.com>
2022-06-15 16:21:53 +0200

    Leksicka analiza

--- pp_projekat/src/rs/ac/bg/etf/pp1/sym.java
+++ pp_projekat/src/rs/ac/bg/etf/pp1/sym.java
@@ -2,4 +2,67 @@ package rs.ac.bg.etf.pp1;
 
 public class sym {
 
+	// keywords
+	public static final int PROGRAM = 1;
+	public static final int BREAK = 2;
+	public static final int CLASS = 3;
+	public static final int ENUM = 4;
+	public static final int ELSE = 5;
+	public static final int CONST = 6;
+	public static final int IF = 7;
+	public static final int DO = 8;
+	public static final int WHILE = 9;
+	public static final int NEW = 10;
+	public static final int PRINT = 11;
+	public static final int READ = 12;
+	public static final int RETURN = 13;
+	public static final int VOID = 14;
+	public static final int EXTENDS = 15;
+	public static final int CONTINUE = 16;
+	public static final int THIS = 17;
+	public static final int SUPER = 18;
+	public static final int GOTO = 19;
+	public static final int RECORD = 20;
+	
+	// identifiers
+	public static final int IDENT = 21;
+	
+	// constants
+	public static final int NUMBER = 22;
+	public static final int CHARCONST = 23;
+	public static final int BOOLCONST = 24;
+
+	// operators
+	public static final int PLUS = 25;
+	public static final int MINUS = 26;
+	public static final int MUL = 27;
+	public static final int DIV = 28;
+	public static final int MOD = 29;
+	public static final int EQUAL = 30;
+	public static final int NOTEQUAL = 31;
+	public static final int GREATER = 32;
+	public static final int GREATEREQUAL = 33;
+	public static final int LOWER = 34;
+	public static final int	LOWEREQUAL = 35;
+	public static final int AND = 36;
+	public static final int OR = 37;
+	public static final int ASSIGN = 38;
+	public static final int INCREMENT = 39;
+	public static final int DECREMENT = 40;
+	public static final int SEMICOLON = 41;
+	public static final int COLON = 42;
+	public static final int COMMA = 43;
+	public static final int DOT = 44;
+
+	public static final int LPAREN = 45;
+	public static final int RPAREN = 46;
+	public static final int LBRACE = 47;
+	public static final int RBRACE = 48;
+	public static final int LBRACECURLY = 49;
+	public static final int RBRACECURLY = 50;
+
+	// comments
+	public static final int COMMENT = 51;
+	
+	public static final int EOF = 52;
 }
